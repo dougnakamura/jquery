1641bfef9bd22a39c0f6d44c32e97345258f89cd
Douglas Batista <101227908+dougnakamura@users.noreply.github.com>
2023-06-21 13:09:14 -0300

    2

--- main.js
+++ main.js
@@ -8,6 +8,16 @@ $(document).ready(function() {
     })
 
     $('form').on('submit',function(e){
-        e.preventDefault();
+        e.preventDafault();
+        const endercoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li></li>');
+        $(`<img src="${endercoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${endercoDaNovaImagem}" target="_blanck" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                    </a>
+            </div>
+        `).appendTo(novoItem)
     })
 })
